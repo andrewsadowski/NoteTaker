c464f6f1b41ae1ee3884193a3e7de1fa644b1fa0
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-10 18:58:51 -0700

    added notes array to js file

--- notes.js
+++ notes.js
@@ -1,3 +1,18 @@
+const notes = [
+  {
+    title: 'My next Trip',
+    body: 'something or other'
+  },
+  {
+    title: 'Habits to work on',
+    body: 'Find habits, work on them'
+  },
+  {
+    title: 'Note 3',
+    body: 'Design World'
+  }
+];
+
 const p = document.querySelector('p');
 const ps = document.querySelectorAll('p');
 
