3664a2059e4533b55113c9ceb547a015bc2c23e3
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-10 20:01:22 -0700

    refactored note function

--- notes.js
+++ notes.js
@@ -13,15 +13,4 @@ const notes = [
   }
 ];
 
-const p = document.querySelector('p');
-const ps = document.querySelectorAll('p');
-
-console.log(p);
-
-ps.forEach(p => {
-  console.log(p.textContent);
-});
-
-const newParagraph = document.createElement('p');
-newParagraph.textContent = 'This is a new element from JS';
-document.querySelector('body').appendChild(newParagraph);
+document.querySelector('button').addEventListener();
