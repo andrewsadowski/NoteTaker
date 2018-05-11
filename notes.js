e5d1f938bcdef0a84b139a21aa8099bbf23a7040
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-10 18:57:54 -0700

    add element to DOM on interaction

--- notes.js
+++ notes.js
@@ -6,3 +6,7 @@ console.log(p);
 ps.forEach(p => {
   console.log(p.textContent);
 });
+
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'This is a new element from JS';
+document.querySelector('body').appendChild(newParagraph);
