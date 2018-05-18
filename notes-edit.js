e3a6b4f10b93fdf57656fe51ae6a98485ef27ae8
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-17 20:57:39 -0700

    added window eventListener to listen for storage event and update accordingly

--- notes-edit.js
+++ notes-edit.js
@@ -1,11 +1,9 @@
-const noteID = location.hash.substring(1);
-const notes = getSavedNotes();
-
 const titleElement = document.querySelector('#note-title');
 const bodyElement = document.querySelector('#note-body');
 const removeNoteButton = document.querySelector('#remove-note');
-
-const note = notes.find(note => {
+const noteID = location.hash.substring(1);
+let notes = getSavedNotes();
+let note = notes.find(note => {
   return note.id === noteID;
 });
 
@@ -30,4 +28,20 @@ removeNoteButton.addEventListener('click', e => {
   removeNote(note.id);
   saveNotes(notes);
   location.assign('/index.html');
+});
+
+window.addEventListener('storage', e => {
+  if (e.key === 'notes') {
+    notes = JSON.parse(e.newValue);
+    note = notes.find(note => {
+      return note.id === noteID;
+    });
+
+    if (note === undefined) {
+      location.assign('/index.html');
+    }
+
+    titleElement.value = note.title;
+    bodyElement.value = note.body;
+  }
 });
